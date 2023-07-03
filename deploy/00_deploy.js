5e4838f67ecafc5dbbe2b3ebb211bba215cb699d
Aayush Gupta <guptaaayush432@gmail.com>
2023-07-03 16:06:08 +0530

    [script] created deploy script

---
+++ deploy/00_deploy.js
@@ -0,0 +1,21 @@
+require("hardhat-deploy")
+require("hardhat-deploy-ethers")
+
+const { networkConfig } = require("../helper-hardhat-config")
+
+const private_key = network.config.accounts[0]
+const wallet = new ethers.Wallet(private_key, ethers.provider)
+
+module.exports = async ({ deployments }) => {
+    const { deploy } = deployments
+    console.log("Wallet Ethereum Address:", wallet.address)
+    const chainId = network.config.chainId
+    console.log("chainID: ", chainId)
+
+    // deploy Check
+    const check = await deploy("Check", {
+        from: wallet.address,
+        args: [],
+        log: true,
+    })
+}
