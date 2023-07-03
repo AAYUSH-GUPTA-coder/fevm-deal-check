ba4d63f150f5e224561d343f4a19c3792acef18e
Aayush Gupta <guptaaayush432@gmail.com>
2023-07-03 16:34:57 +0530

    [task] cleaned the task script

--- tasks/check-deal.js
+++ tasks/check-deal.js
@@ -1,13 +1,9 @@
-// const CID = require("cids")
-
 task("check-deal", "check deal activation from deal ID")
     .addParam("contract", "The address of the check contract solidity")
     .addParam("dealId", "The deal ID of the deal you want the status of")
     .setAction(async (taskArgs) => {
         const contractAddr = taskArgs.contract
         const dealId = taskArgs.dealId
-        // const cidHexRaw = new CID(cid).toString("base16").substring(1)
-        // const cidHex = "0x" + cidHexRaw
         const networkId = network.name
         console.log("Getting deal status on network", networkId)
 
@@ -21,33 +17,31 @@ task("check-deal", "check deal activation from deal ID")
         const check = await Check.attach(contractAddr)
 
         //send a transaction to call checkDealStatus() method
-        //transaction = await check.getDealProposal(proposalID)
+        // --- Calling checkDealStatus function ---
         let result = await check.checkDealStatus(dealId, {
             gasLimit: 1000000, //797263
         })
         console.log("The status of deal is:", result)
-        console.log("The status of deal is:", result.terminated)
-        console.log("The status of deal is:", result.activated)
 
+        // --- Calling checkDealStatusData function  ----
         let fileDataResult = await check.checkDealStatusData(dealId, {
             gasLimit: 1000000, //797263
         })
         console.log("The status of deal is:", fileDataResult)
-        console.log("The status of deal is:", fileDataResult.terminated)
-        console.log("The status of deal is:", fileDataResult.activated)
 
+        // --- Calling checkDealStatusData1 function ---
         let fileDataResult1 = await check.checkDealStatusData1(dealId, {
             gasLimit: 1000000, //797263
         })
         console.log("The status of deal is:", fileDataResult1)
-        console.log("The status of deal is:", fileDataResult1.terminated)
-        console.log("The status of deal is:", fileDataResult1.activated)
 
+        // --- Calling getTerminalTime function -------
         let terminalTime = await check.getTerminalTime(dealId, {
             gasLimit: 1000000, //797263
         })
         console.log("The Terminal Time is:", terminalTime)
-
+        
+        // --- Calling getTerminalTimeEpoch function -------
         let TerminalTimeEpoch = await check.getTerminalTimeEpoch(dealId, {
             gasLimit: 1000000, //797263
         })
